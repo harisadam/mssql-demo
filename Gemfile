bc8927407b50c43d7cca6715166d96ca9253b1da
Adam Haris <iamadamharis@gmail.com>
2023-01-17 08:54:29 +0100

    add mssql connection example

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem "redis", "~> 4.0"
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 gem "tiny_tds"
+gem "tailwindcss-rails"
 
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
---
+++ app/controllers/bilboards_controller.rb
@@ -0,0 +1,7 @@
+class BilboardsController < ApplicationController
+  def index
+    #Define MSSQL connection logic here
+    #client = TinyTds::Client.new username: 'sa', password: 'secret', host: 'mydb.host.net'
+    #@result = client.execute("SELECT * FROM [datatypes]")
+  end
+end
